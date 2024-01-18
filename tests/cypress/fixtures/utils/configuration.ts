8f8bd1a1c6d23b8ac87f4b7385813c0614cfea1f
Geofrey Flores <75278792+gflores-jahia@users.noreply.github.com>
2024-01-18 10:12:53 -0500

    BACKLOG-22218: Add more test scenarios (#12)

--- tests/cypress/fixtures/utils/configuration.ts
+++ tests/cypress/fixtures/utils/configuration.ts
@@ -1,5 +1,7 @@
 import gql from 'graphql-tag';
 
+const pid = 'org.jahia.modules.richtext.config';
+
 export const installConfig = configFilePath => {
     return cy.runProvisioningScript(
         {fileContent: `- installConfiguration: "${configFilePath}"`, type: 'application/yaml'},
@@ -8,7 +10,6 @@ export const installConfig = configFilePath => {
 };
 
 export const getConfig = siteKey => {
-    const pid = 'org.jahia.modules.richtext.config';
     const getConfigGql = gql`
         query getConfig($pid: String!, $siteKey: String!) {
             admin {
@@ -23,5 +24,23 @@ export const getConfig = siteKey => {
     return cy.apollo({
         query: getConfigGql,
         variables: {pid, siteKey}
+    });
+};
+
+export const addConfig = (key, value, siteKey = 'default') => {
+    const addConfigGql = gql`
+        mutation addConfig($pid: String!, $identifier: String!, $key: String!, $value: String!) {
+            admin {
+                jahia {
+                    configuration(pid: $pid, identifier: $identifier) {
+                        value(name: $key value: $value)
+                    }
+                }
+            }
+        }
+    `;
+    return cy.apollo({
+        mutation: addConfigGql,
+        variables: {pid, identifier: siteKey, key, value}
     });
 };

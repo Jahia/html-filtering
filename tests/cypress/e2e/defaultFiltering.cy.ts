e876c42f08e418dedc483507a3a5c4d17a0398fe
Geofrey Flores <gflores@jahia.com>
2024-01-19 16:43:42 -0500

    Fix test

--- tests/cypress/e2e/defaultFiltering.cy.ts
+++ tests/cypress/e2e/defaultFiltering.cy.ts
@@ -30,20 +30,6 @@ describe('Default HTML filtering', () => {
         deleteNode(path);
     });
 
-    it('allows internal links - content', () => {
-        // Note that the actual href text being sent over to the sanitizer is '##cms-context##/{mode}/{lang}/##ref:link1##'
-        const text = '<p><a href="/cms/{mode}/{lang}/sites/mySite/home/search-results.html" title="search-results">search results</a></p>';
-        modifyContent(path, text);
-        getContent(path).then(result => {
-            const value = result.data.jcr.nodeByPath.property.value;
-            expect(value).to.contain('<p>');
-            expect(value).to.contain('<a');
-            expect(value).to.contain('href');
-            expect(value).to.contain('{mode}');
-            expect(value).to.contain('{lang}');
-        });
-    });
-
     it('allows internal links - files', () => {
         // Note that the actual href text being sent over to the sanitizer is '##doc-context##/{workspace}/##ref:link1##'
         const text = '<p><a href="/files/{workspace}/sites/digitall/files/images/pdf/Conference%20Guide.pdf" ' +
@@ -56,6 +42,20 @@ describe('Default HTML filtering', () => {
             expect(value).to.contain('href');
             expect(value).to.contain('title');
             expect(value).to.contain('{workspace}');
+        });
+    });
+
+    it('allows internal links - content', () => {
+        // Note that the actual href text being sent over to the sanitizer is '##cms-context##/{mode}/{lang}/##ref:link1##'
+        const text = '<p><a href="/cms/{mode}/{lang}/sites/digitall/home/search-results.html" title="search-results">search results</a></p>';
+        modifyContent(path, text);
+        getContent(path).then(result => {
+            const value = result.data.jcr.nodeByPath.property.value;
+            expect(value).to.contain('<p>');
+            expect(value).to.contain('<a');
+            expect(value).to.contain('href');
+            expect(value).to.contain('{mode}');
+            expect(value).to.contain('{lang}');
         });
     });
 

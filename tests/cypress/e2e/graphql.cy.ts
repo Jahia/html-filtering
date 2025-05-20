031181b9df6ba3836d65b0eabefadc7d34108da5
Volodymyr Tereshchuk <vtereshchuk@jahia.com>
2025-05-20 17:24:26 +0200

    update html string to be tested (#92)

--- tests/cypress/e2e/graphql.cy.ts
+++ tests/cypress/e2e/graphql.cy.ts
@@ -1,33 +1,36 @@
 describe('Graphql test', () => {
-    it('Should return a valid content', () => {
-        const html = '<p>value</p>';
+    const safeHTML = '<p>value</p>';
+    const unsafeHTML = '<badTag badTagAttribute>value</badTag><p class="myClass" badPAttribute>test</p><script>alert(document.location)</script>';
+    const sanitizedUnsafeHTML = 'value<p class="myClass">test</p>';
+    it('Should return a safe content', () => {
         cy.apollo({
             queryFile: 'graphql/graphqlTest.graphql',
             variables: {
-                html
+                html: safeHTML
             }
         }).its('data.htmlFiltering.validate').should(res => {
             expect(res.removedTags).to.be.empty;
             expect(res.removedAttributes).to.be.empty;
-            expect(res.sanitizedHtml).to.equal(html);
+            expect(res.sanitizedHtml).to.equal(safeHTML);
             expect(res.safe).to.be.true;
         });
     });
 
-    it('Should return an invalid content', () => {
-        const html = '<badTag badTagAttribute>value</badTag><p badPAttribute>test</p>';
+    it('Should return sanitized unsafe content', () => {
         cy.apollo({
             queryFile: 'graphql/graphqlTest.graphql',
             variables: {
-                html
+                html: unsafeHTML
             }
         }).its('data.htmlFiltering.validate').should(res => {
-            expect(res.removedTags.length).to.equal(1);
+            expect(res.removedTags.length).to.equal(2);
             expect(res.removedTags).to.contain('badtag');
+            expect(res.removedTags).to.contain('script');
             expect(res.removedAttributes.length).to.equal(1);
-            expect(res.removedAttributes[0].attributes).to.contain('badpattribute');
             expect(res.removedAttributes[0].tag).to.equal('p');
-            expect(res.sanitizedHtml).to.equal('value<p>test</p>');
+            expect(res.removedAttributes[0].attributes).to.contain('badpattribute');
+            expect(res.removedAttributes[0].attributes).to.not.contain('class');
+            expect(res.sanitizedHtml).to.equal(sanitizedUnsafeHTML);
             expect(res.safe).to.be.false;
         });
     });
